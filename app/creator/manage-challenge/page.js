b65c795f7404a2e1143dd9314c69a7230d7ed0a7
Caryn Chan <carynchan21@gmail.com>
2023-10-13 20:05:00 +0800

    feat: add challenge

--- app/creator/manage-challenge/page.js
+++ app/creator/manage-challenge/page.js
@@ -1,9 +1,12 @@
+"use client"
 import ChallengeResults from "@/components/ChallengeResults"
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
   return (
     <section>
-      <ChallengeResults/>
+      <ChallengeResults router={router}/>
     </section>
   )
 }
