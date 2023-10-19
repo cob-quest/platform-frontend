e7cedf1b626c102d94f60212ee570a143c6f7386
Caryn Chan <carynchan21@gmail.com>
2023-10-19 22:14:48 +0800

    feat: upload image

--- app/creator/create-challenge/page.js
+++ app/creator/create-challenge/page.js
@@ -1,30 +1,200 @@
+// import React from "react";
 
-import React from "react";
+// export default function CreateChallenge() {
+//   return (
+//     <main>
+//       <div>
+//         <h2 className="underline">
+//           <i>Retrieve image by typing the name of the image </i>
+//         </h2>
+//       </div>
+//       <div>
+//         <h2 className="underline">
+//           <i>Enter time allocated for challenge</i>
+//         </h2>
+//         <p> Time format HH:MM </p>
+//       </div>
+//       <div>
+//         <h2 className="underline">
+//           <i>Enter the Email IDs of the Challenge Participants</i>
+//         </h2>
+//       </div>
+//     </main>
+//   );
+// }
+
+// // drag drop (zip file)
+// // mailing list (but idk the format yet)
+// // set time (but idk the format yet)
+
+"use client";
+import React, { useState } from "react";
+import Help from "../../../components/creator/CreatorHelp";
+import { useRouter } from "next/navigation";
+
+const CreatorTerminal = () => {
+  const [input, setInput] = useState("");
+  const [output, setOutput] = useState("");
+  const [history, setHistory] = useState([]);
+
+  const router = useRouter();
+
+  const handleInputChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    setOutput("");
+
+    // Process input command
+    let newOutput = "";
+    if (input === "help") {
+      newOutput = <Help />;
+    } else if (input === "retrieve image") {
+      // name of image
+      let isDone = false;
+      newOutput = (
+        <p>
+          <span className="user">[✔]</span> Enter the image you would like to
+          retrieve using the correlation ID.... when you are done, type "
+          <span className="commands">done</span>".
+        </p>
+      );
+      // Continuously prompt for input until the user enters "done"
+      while (!isDone) {
+        // Use the "await" keyword to asynchronously wait for input
+        const inputResult = await getUserInput();
+
+        if (inputResult === "done") {
+          isDone = true;
+        } else {
+          // Handle the input, e.g., store it or perform actions
+          // You can add logic to process each entered value here
+        }
+      }
+      // TODO: add image retrieval logic
+        // image does not exist
+    } else if (input === "timer") {
+      newOutput = (
+        <p>
+          <span className="user">[✔]</span> Enter the time that you would like
+          to allot for this challenge....
+        </p>
+      );
+      // TODO: add input timer logic
+    } else if (input === "participants") {
+      newOutput = (
+        <p>
+          <span className="user">[✔]</span> Enter the list of participants
+          (seperated by commas)....
+        </p>
+      );
+    } else if (input === "cd") {
+      newOutput = (
+        <p>
+          <span className="user">[✔]</span> ....
+        </p>
+      );
+      router.push("/");
+    } else if (input === "clear") {
+      setHistory([]);
+      setInput("");
+      return;
+    } else {
+      newOutput = (
+        <p>
+          Command not found. Type "<span className="commands">help</span>" for
+          available commands.
+        </p>
+      );
+    }
+    setOutput(newOutput);
+    setHistory((prevHistory) => [...prevHistory, { input, output: newOutput }]);
+    setInput("");
+  };
+
+   // Asynchronous function to get user input
 
 
-export default function CreateChallenge() {
+  // const renderOutput = () => {
+  //     if (output === '') {
+  //         return null;
+  //     }
+
+  //     return (
+  //         <pre className="output">
+  //             <code>{output}</code>
+  //         </pre>
+  //     );
+  // };
+
+  const renderHistory = () => {
+    if (history.length === 0) {
+      return null;
+    }
+
+    return (
+      <div className="history">
+        {history.map((item, index) => (
+          <div key={index} className="history">
+            <span>
+              <span className="commands">
+                creator
+                <span className="symbols">@</span>
+                <span className="user">cob.dev:</span>
+                <span className="symbols">~$ </span>
+              </span>
+              <span className="commands">{item.input}</span>
+            </span>
+            <br />
+            <span className="output">{item.output}</span>
+            <div className="spacing"></div>
+          </div>
+        ))}
+      </div>
+    );
+  };
+
   return (
-    <main>
-      <div>
-        <h2 className="underline">
-          <i>Retrieve image</i>
-        </h2>
-      </div>
-      <div>
-        <h2 className="underline">
-          <i>Enter time allocated for challenge</i>
-        </h2>
-        <p> Time format HH:MM </p>
-      </div>
-      <div>
-        <h2 className="underline">
-          <i>Enter the Email IDs of the Challenge Participants</i>
-        </h2>
-      </div>
-    </main>
-  );
-}
+    <div className="terminal">
+      {/* {renderOutput()} */}
+      <span className="commands">
+        creator
+        <span className="symbols">@</span>
+        <span className="user">cob.dev:</span>
+        <span className="symbols">~$</span>
+        <span className="commands"> welcome@creator</span>
+      </span>
+      <p>
+        {" "}
+        3 steps to create a challenge: '
+        <span className="commands">retrieve image</span>' -&gt;{" "}
+        '<span className="commands">timer</span>' -&gt;{" "}
+        '<span className="commands">participants</span>'
+      </p>
 
-// drag drop (zip file)
-// mailing list (but idk the format yet)
-// set time (but idk the format yet)
+      {/* <Help /> */}
+      {renderHistory()}
+      <div className="terminal-spacing">
+        <span className="commands">
+          creator
+          <span className="symbols">@</span>
+          <span className="user">cob.dev:</span>
+          <span className="symbols">~$ </span>
+        </span>
+        <form onSubmit={handleFormSubmit}>
+          <input
+            type="text"
+            value={input}
+            onChange={handleInputChange}
+            autoFocus
+            className="input-text-custom commands"
+          />
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default CreatorTerminal;
--- components/file-upload/FileUpload.js
+++ components/file-upload/FileUpload.js
@@ -1,15 +1,38 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { useDropzone } from "react-dropzone";
 import { CursorArrowRaysIcon } from "@heroicons/react/24/solid";
+import axios from "axios";
 
 const FileUpload = () => {
-  const onDrop = (acceptedFiles) => {
-    // Handle the uploaded files (e.g., send them to a server)
-    acceptedFiles.forEach((file) => {
-      console.log(`Uploaded file: ${file.name}`);
-      // upload to dest
-    });
+  const [uploadedFileNames, setUploadedFileNames] = useState([]); // State to store uploaded file names
+
+  const onDrop = async (acceptedFiles) => {
+    const formData = new FormData();
+    formData.append("file", acceptedFiles[0]);
+
+    try {
+      const response = await axios.post(
+        "http://localhost:80/api/v1/trigger/image/upload",
+        formData,
+        {
+          auth: {
+            username: "test",
+            password: "test",
+          },
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+
+      console.log("File upload response:", response.data);
+
+      // Add the uploaded file name to the state
+      setUploadedFileNames([...uploadedFileNames, acceptedFiles[0].name]);
+    } catch (error) {
+      console.error("File upload error:", error);
+    }
   };
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
@@ -21,15 +44,26 @@ const FileUpload = () => {
     >
       <input {...getInputProps()} />
       {isDragActive ? (
-        <p>drop the files here...</p>
+        <p>Drop the files here...</p>
       ) : (
         <p>
-          drag 'n' drop some ZIP files here, or click to select files{" "}
+          Click to select your files and create image{" "}
           <span>
             {" "}
             <CursorArrowRaysIcon className="h-4 w-4" />{" "}
           </span>{" "}
         </p>
+      )}
+
+      {uploadedFileNames.length > 0 && (
+        <div>
+          <p>Uploaded files: </p>
+          <ul>
+            {uploadedFileNames.map((fileName, index) => (
+              <li key={index}>{fileName}</li>
+            ))}
+          </ul>
+        </div>
       )}
     </div>
   );
