b0de465e7d7aa3124391f45b62fa37c288c75acb
Caryn Chan <carynchan21@gmail.com>
2023-10-21 22:13:15 +0800

    fix: change endpoint for upload

--- components/file-upload/FileUpload.js
+++ components/file-upload/FileUpload.js
@@ -31,7 +31,7 @@ const FileUpload = () => {
 
     try {
       const response = await axios.post(
-        "http://localhost:80/api/v1/trigger/image/upload",
+        "http://localhost:80/api/v1/trigger/image",
         formData,
         {
           headers: {
