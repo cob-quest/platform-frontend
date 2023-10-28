1f9ffcdadde4a68963c24e6d5eff24ac2559d83b
Caryn Chan <carynchan21@gmail.com>
2023-10-28 19:06:26 +0800

    feat: list creator challenges

--- components/creator/CreatorTerminal.jsx
+++ components/creator/CreatorTerminal.jsx
@@ -7,6 +7,7 @@ const CreatorTerminal = () => {
   const [input, setInput] = useState("");
   const [output, setOutput] = useState("");
   const [history, setHistory] = useState([]);
+  const [challengeNames, setChallengeNames] = useState([]);
 
   const router = useRouter();
 
@@ -14,7 +15,7 @@ const CreatorTerminal = () => {
     setInput(e.target.value);
   };
 
-  const handleFormSubmit = (e) => {
+  const handleFormSubmit = async (e) =>  {
     e.preventDefault();
     setOutput("");
 
@@ -51,11 +52,41 @@ const CreatorTerminal = () => {
     } else if (input === "my challenges") {
       newOutput = (
         <p>
-          <span className="user">[✔]</span> Opening your challenges....
+          <span className="user">[✔]</span> Listing your challenges....
         </p>
       );
-      const newTab = window.open("/creator/my-challenges", "_blank");
-      newTab.focus();
+      try {
+        const response = await axios.get(
+          "http://localhost:80/api/v1/platform/challenge",
+          {
+            headers: {
+              Authorization: "Basic " + btoa("test:test"), // Replace with your authorization token
+            },
+          }
+        );
+        // Iterate through the responseData to display image names
+        // Extract image names and set them in the state
+        const responseData = response.data;
+        const responseStatus = response.status;
+        console.log("Response Data:", responseData);
+        console.log("Response Status:", responseStatus);
+
+        const challengeNames = responseData.map((challenge) => challenge.challengeName);
+        setChallengeNames(challengeNames);
+        console.log("Challenge names:", challengeNames);
+        newOutput = (
+          <div>
+            {challengeNames.map((name, index) => (
+              <p key={index}>{name}</p>
+            ))}
+          </div>
+        );
+        // Update state or perform further actions as needed with the responseData.
+      } catch (error) {
+        console.error("Error fetching images:", error);
+        // Handle errors as needed
+      }
+        // LIST CHALLENGES
     } else if (input === "cd") {
       newOutput = (
         <p>
@@ -123,7 +154,7 @@ const CreatorTerminal = () => {
         would you like to '<span className="commands">create image</span>' or '
         <span className="commands">create challenge</span>' or '
         <span className="commands">manage challenge</span>' or '
-        <span className="commands">my challenge</span>'?
+        <span className="commands">my challenges</span>'?
       </p>
       {/* <Help /> */}
       {renderHistory()}
